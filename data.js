4101bdd76b95ab7fb1678f565fbe1e2a05786d7b
Aram Papikyan <onlyphilosophylover@gmail.com>
2025-02-01 13:27:44 +0200

    exp

---
+++ data.js
@@ -0,0 +1,15 @@
+export const MeDevInfo =
+  "Я Frontend разработчик, специализирующийся на создании приложений на React. Ориентирован на чистый, поддерживаемый код и оптимизацию производительности приложений. Активно ищу возможности для профессионального роста и работы в команде.";
+
+export const instrumentsData = [
+  "React + TypeScript",
+  "Redux",
+  "JavaScript (ES6+)",
+  "TailwindCSS",
+  "SCSS",
+  "CSS3",
+  "HTML5",
+  "PHP - для отправки форм",
+  "Vite",
+  "Webpack"
+];
